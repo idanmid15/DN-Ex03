b6ad75cb1a7da31d10ba4c153404d47734ed6a28
eyalsolomon10 <eyal.solomon@post.idc.ac.il>
2016-05-20 13:51:56 +0300

    Engine class
    includes: engine type (enum), rePower method

---
+++ B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Engine
+    {
+        public enum eEngineType
+        {
+            fuel,
+            electric,
+            other
+        }
+
+        public abstract void rePower(float powerAmount);
+    }
+}
