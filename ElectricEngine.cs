5e27a295e78a5f53a18c8d108a59d0ee53ddbb8c
eyalsolomon10 <eyal.solomon@post.idc.ac.il>
2016-05-20 14:50:52 +0300

    added Bike class not done
    changes of getters in all classes

---
+++ ElectricEngine.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex03.GarageLogic
+{
+    public class ElectricEngine : Engine
+    {
+        private float m_ChargeTimeLeft;
+        private float r_MaxChargeTime;
+        private eEngineType m_EngineType;
+
+        public ElectricEngine(float i_ChargeTimeLeft, float i_MaxChargeTime, eEngineType i_EngineType)
+        {
+            this.m_ChargeTimeLeft = i_ChargeTimeLeft;
+            this.r_MaxChargeTime = i_MaxChargeTime;
+            this.m_EngineType = i_EngineType;
+        }
+
+        public override void RePower(float i_PowerAmount)
+        {
+            if (i_PowerAmount + this.m_ChargeTimeLeft > this.r_MaxChargeTime)
+            {
+                throw new Exception;
+            }
+            else
+            {
+                this.m_ChargeTimeLeft += i_PowerAmount;
+            }
+        }
+    }
+}
