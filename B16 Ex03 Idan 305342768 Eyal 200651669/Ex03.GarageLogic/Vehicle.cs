ecb3aeae0783b540fe1c185c0c9b0ce441f9edf3
eyalsolomon10 <eyal.solomon@post.idc.ac.il>
2016-05-20 13:46:47 +0300

    vehicle class
    only members

---
+++ B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex03.GarageLogic
+{
+    public class Vehicle
+    {
+        public string m_ModelType;
+        public string m_LicensePlate;
+        public float m_EnergyPrecentageLeft;
+        public List<Wheel> m_Wheels;
+    }
+}
