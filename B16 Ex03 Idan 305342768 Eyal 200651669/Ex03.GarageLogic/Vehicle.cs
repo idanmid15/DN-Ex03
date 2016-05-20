5aa4fd89589c4a8520da7b580a2a53d844eb6fb5
eyalsolomon10 <eyal.solomon@post.idc.ac.il>
2016-05-20 14:37:07 +0300

    Add files via upload

--- B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/Vehicle.cs
+++ B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/Vehicle.cs
@@ -5,12 +5,32 @@ namespace Ex03.GarageLogic
 {
     public class Vehicle
     {
-        private string m_ModelType { get; }
+        private string m_ModelType;
 
-        private string m_LicensePlate { get; }
+        private string m_LicensePlate;
 
-        private float m_EnergyPrecentageLeft { get; set; }
+        private float m_EnergyPrecentageLeft;
 
-        public List<Wheel> m_Wheels { get; }
+        public List<Wheel> m_Wheels;
+
+        public string GetModelType()
+        {
+            return m_ModelType;
+        }
+
+        public string GetLicensePlatel()
+        {
+            return m_LicensePlate;
+        }
+
+        public float GetEnergyPrecentageLeft()
+        {
+            return m_EnergyPrecentageLeft;
+        }
+
+        public List<Wheel> GetWheels()
+        {
+            return m_Wheels;
+        }
     }
 }
--- B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/Wheel.cs
+++ B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/Wheel.cs
@@ -1,34 +1,20 @@
 ﻿namespace Ex03.GarageLogic
 {
-using System;
-using System.Collections.Generic;
-using System.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
 
     public class Wheel
     {
+        private string m_Manufacturer { get; }
+
+        private float m_CurrentTirePressure { get; set; }
+
         private readonly float r_MaxTirePressure;
-        public string m_Manufacturer;
 
-        private float m_CurrentTirePressure;
-
-        public float GetMaxTirePressure()
+        public float getMaxTirePressure()
         {
             return r_MaxTirePressure;
-        }
-        
-        public string GetManufacturer()
-        {
-            return m_Manufacturer;
-        }
-
-        public float GetCurrentTirePressure()
-        {
-            return m_CurrentTirePressure;
-        }
-
-        public void SetTirePressure(float i_TirePressure)
-        {
-            this.m_CurrentTirePressure = i_TirePressure;
         }
 
         public Wheel(string i_Manufacturer, float i_TirePressure, int i_MaxTirePressure)
