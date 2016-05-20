47ff58977b6e1bdd266199b0741f52a7c1d345d6
idanmid15 <idan.midroni@post.idc.ac.il>
2016-05-20 16:27:52 +0300

    Add files via upload

--- B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/ElectricBike.cs
+++ B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/ElectricBike.cs
@@ -3,21 +3,21 @@ namespace Ex03.GarageLogic
 {
     public class ElectricBike : Bike
     {
+        private const float k_MaxChargeTime = 1.9f;
+
         public ElectricBike
             (string i_ModelType,
             string i_LicensePlate,
             string i_WheelManufacturer,
             float[] i_TirePressures,
-            float i_ChargeTimeLeft,
-            float i_MaxChargeTime)
+            float i_ChargeTimeLeft)
         {
             this.m_ModelType = i_ModelType;
             this.m_LicensePlate = i_LicensePlate;
             this.m_Wheels.Add(new Wheel(i_WheelManufacturer, i_TirePressures[0], k_MaxTirePressure));
             this.m_Wheels.Add(new Wheel(i_WheelManufacturer, i_TirePressures[1], k_MaxTirePressure));
-            this.m_Engine = new ElectricEngine(i_ChargeTimeLeft, i_MaxChargeTime);
-
-
+            this.m_Engine = new ElectricEngine(i_ChargeTimeLeft, k_MaxChargeTime);
+            this.m_EnergyPrecentageLeft = (1 - i_ChargeTimeLeft / k_MaxChargeTime);
         }
     }
 }
